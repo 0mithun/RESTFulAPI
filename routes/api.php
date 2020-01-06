8ddf2a61bc8fa51fb9c117750d5ab569dfbe1533
Mithun Halder <halderm86@gmail.com>
2020-01-06 15:23:34 +0600

    Creating Buyer, Seller, Product, Category, Transaction, User models & creating api routes

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,27 @@ use Illuminate\Http\Request;
 //     return $request->user();
 // });
 
-    Route::middleware('auth:api')->get('/user', function (Request $request) {
-        return $request->user();
-    });
-    
+    // Route::middleware('auth:api')->get('/user', function (Request $request) {
+    //     return $request->user();
+    // });
+
+    //Buyers
+    Route::apiResource('buyers', 'Buyer\BuyerController')->only(['index','show']);
+
+
+    //Categories
+    Route::apiResource('categories', 'Category\CategoryController');
+
+
+    //Products
+    Route::apiResource('products', 'Product\ProductController')->only(['index','show']);
+
+    //Transactions
+    Route::apiResource('transactions', 'Transaction\TransactionController')->only(['index','show']);
+
+    //Users
+    Route::apiResource('users', 'User\UserController')->only(['index','show']);
+
+    //Sellers
+    Route::apiResource('sellers', 'Seller\SellerController');
+
