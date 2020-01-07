71283c3e4aeb5b145be756750bc9954b70ec6b33
Mithun Halder <halderm86@gmail.com>
2020-01-07 15:48:43 +0600

    Creating Factory

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\User;
+use App\Product;
+use App\Category;
+use App\Transaction;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use SebastianBergmann\Environment\Console;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +17,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+         //$this->call(UsersTableSeeder::class);
+
+        //DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+
+        //User::turncate();
+        // Category::turncate();
+        // Product::turncate();
+        // Transaction::turncate();
+        // DB::table('category_product')->turncate();
+
+        $userQuantity = 20;
+        $categoryQuantity = 30;
+        $productQuantity = 200;
+        $transactionQuantity = 50;
+
+        
+        factory(User::class, $userQuantity)->create();
+        factory(Category::class, $categoryQuantity)->create();
+        factory(Product::class, $productQuantity)->create()->each(
+            function($product){
+                $categories = Category::all()->random(mt_rand(1, 5))->pluck('id');
+                $product->categories()->attach($categories);
+            }
+        );
+        factory(Transaction::class, $transactionQuantity)->create();
+
+
     }
 }
