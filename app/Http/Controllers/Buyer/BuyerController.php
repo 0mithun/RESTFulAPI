80b80e527f8fa69cec8f9025d3cf592b3cbfa77c
Mithun Halder <halderm86@gmail.com>
2020-01-12 23:28:17 +0600

    Buyer & Seller api request added

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Buyer;
 
+use App\Buyer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,9 @@ class BuyerController extends Controller
      */
     public function index()
     {
-        //
+        $buyers = Buyer::has('transaction')->get();
+
+        return response()->json(['data'=>$buyers], 200);
     }
 
     /**
@@ -36,7 +39,8 @@ class BuyerController extends Controller
      */
     public function show($id)
     {
-        //
+        $buyer = Buyer::has('transaction')->findOrFail($id);
+        return response()->json(['data'=>$buyer], 200);
     }
 
     /**
