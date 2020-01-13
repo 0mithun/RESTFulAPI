b4a3e2bddeb15f76dc62c5bbccc15da675ed37c9
Mithun Halder <halderm86@gmail.com>
2020-01-13 23:05:22 +0600

    Seller & Buyer Global Scope added

--- app/Buyer.php
+++ app/Buyer.php
@@ -2,11 +2,18 @@
 
 namespace App;
 
+use App\Scopes\BuyerScope;
 use Illuminate\Database\Eloquent\Model;
 
 class Buyer extends User
 {
-    public function transaction(){
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new BuyerScope);
+    }
+
+    public function transactions(){
         return $this->hasMany(Transaction::class);
     }
     
